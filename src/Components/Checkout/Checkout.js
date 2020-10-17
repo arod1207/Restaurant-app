1f37f6ed1e0d52d4a887c9c870536475e44d7e0d
Armando Rodriguez <arod1207@me.com>
2020-10-16 22:50:52 -0500

    added a remove button

--- src/Components/Checkout/Checkout.js
+++ src/Components/Checkout/Checkout.js
@@ -1,20 +1,44 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import db from '../../firbase';
+
+import { basketContext } from '../../basketContext';
+
+import Button from '@material-ui/core/Button';
 
 import './Checkout.css';
 
 function Checkout() {
+    const [items, setItems] = useContext(basketContext);
+
     return (
-        <div className="checkout">
-            {/* <Card className="text-center">
-                <Card.Header>Final Order</Card.Header>
-                <Card.Body>
-                    <Card.Title>Your Order</Card.Title>
-                    <Card.Text>Your Orders</Card.Text>
-                    <Button variant="primary">Purchase</Button>
-                </Card.Body>
-                <Card.Footer className="text-muted"></Card.Footer>
-            </Card> */}
-        </div>
+        <>
+            <div className="checkout__title">Checkout</div>
+            <div className="checkout">
+                {items.map((item) => (
+                    <div className="checkout__card" key={item.id}>
+                        <div className="checkout__image">
+                            <img src={item.item.image} alt="" />
+                        </div>
+                        <div className="checkout__name">{item.item.name}</div>
+
+                        <div className="checkout__price">{item.item.price}</div>
+                        <div className="checkout__removeButton">
+                            <Button
+                                variant="contained"
+                                color="secondary"
+                                onClick={(e) =>
+                                    db.collection('Items').doc(item.id).delete()
+                                }
+                            >
+                                Remove
+                            </Button>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <div className="checkout__total">{`${items.price}`}</div>
+        </>
     );
 }
 
