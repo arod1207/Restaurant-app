789b249d3957820f8458f8fa3ac429bfe4708f1f
Arod1207 <arod1207@me.com>
2020-10-19 07:08:44 -0500

    removed history

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -1,95 +1,96 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import firebase from 'firebase';
-import { db } from '../../firebase';
+import firebase from "firebase";
+import { db } from "../../firebase";
 
-import Button from '@material-ui/core/Button';
+import Button from "@material-ui/core/Button";
 
-import './SignUp.css';
+import "./SignUp.css";
 
-function SignUp({ history }) {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [checkPassword, setCheckPassword] = useState('');
-    const [userId, setUserId] = useState('');
+function SignUp() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [checkPassword, setCheckPassword] = useState("");
+  const [userId, setUserId] = useState("");
+  const [userEmail, setUserEmail] = useState("");
 
-    console.log(userId);
+  console.log(userId);
 
-    const handleSignUp = (e) => {
-        e.preventDefault();
+  const handleSignUp = (e) => {
+    e.preventDefault();
 
-        if (password !== checkPassword) {
-            return alert('Passwords do not match');
-        }
+    if (password !== checkPassword) {
+      return alert("Passwords do not match");
+    }
 
-        if (!password) {
-            return alert('Please enter a password1');
-        }
+    if (!password) {
+      return alert("Please enter a password1");
+    }
 
-        firebase
-            .auth()
-            .createUserWithEmailAndPassword(email, password)
-            .then((user) => {
-                setUserId(user.user.uid);
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then((user) => {
+        setUserId(user.user.uid);
+        setUserEmail(user.user.email);
+      })
+      .then(() => {
+        db.collection("Users")
+          .doc(userId)
+          .set({
+            email: userEmail,
+          })
+          .then(() => {
+            alert("User Created");
+            setEmail("");
+            setPassword("");
+            setCheckPassword("");
+          });
+      })
+      .catch((error) => alert(error));
+  };
 
-                db.collection('Users').doc('userId').set({
-                    userId: 'mando',
-                });
-
-                alert('User Created');
-                setEmail('');
-                setPassword('');
-                setCheckPassword('');
-                history.push('/');
-            })
-            .catch((error) => alert(error));
-    };
-
-    return (
-        <div className="signup">
-            <div className="signup__title">Sign Up</div>
-            <form onSubmit={handleSignUp}>
-                <div className="signup__card">
-                    <div className="signup__cardLabel">Email</div>
-                    <div className="signup__cardInput">
-                        <input
-                            type="text"
-                            placeholder="username"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                    </div>
-                    <div className="signup__cardLabel">Password</div>
-                    <div className="signup__cardInput">
-                        <input
-                            type="password"
-                            placeholder="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                    </div>
-                    <div className="signup__cardLabel">Check Password</div>
-                    <div className="signup__cardInput">
-                        <input
-                            type="password"
-                            placeholder="password"
-                            value={checkPassword}
-                            onChange={(e) => setCheckPassword(e.target.value)}
-                        />
-                    </div>
-                    <div className="signup__button">
-                        <Button
-                            type="submit"
-                            variant="contained"
-                            color="primary"
-                        >
-                            Submit
-                        </Button>
-                    </div>
-                </div>
-            </form>
+  return (
+    <div className="signup">
+      <div className="signup__title">Sign Up</div>
+      <form onSubmit={handleSignUp}>
+        <div className="signup__card">
+          <div className="signup__cardLabel">Email</div>
+          <div className="signup__cardInput">
+            <input
+              type="text"
+              placeholder="username"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="signup__cardLabel">Password</div>
+          <div className="signup__cardInput">
+            <input
+              type="password"
+              placeholder="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="signup__cardLabel">Check Password</div>
+          <div className="signup__cardInput">
+            <input
+              type="password"
+              placeholder="password"
+              value={checkPassword}
+              onChange={(e) => setCheckPassword(e.target.value)}
+            />
+          </div>
+          <div className="signup__button">
+            <Button type="submit" variant="contained" color="primary">
+              Submit
+            </Button>
+          </div>
         </div>
-    );
+      </form>
+    </div>
+  );
 }
 
 export default SignUp;
